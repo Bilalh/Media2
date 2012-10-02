ddb9f0d58fd46804e8526795edc6cea6796f0870
Bilal Syed Hussain <bilalshussain@gmail.com>
2012-10-02 21:47:17 +0100

    remove ops

--- main.hs
+++ main.hs
@@ -7,6 +7,10 @@ path="/Users/bilalh/Movies/.Movies/Anime/"
 -- Categorise videos by series and presents a menu for playing them with mplayer
 
 main = do 
+    pid <- play MPlayerOSX
+    return pid
+
+play player = do
     selected <- selectVideosInfo path latest
-    pid <- runCommand $ videoCommand selected
+    pid <- runCommand $ videoCommand player selected
     return pid
