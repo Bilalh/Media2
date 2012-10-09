bb1a4012c2ab4b0226acbede76b89a4db8d50b40
Bilal Syed Hussain <bilalshussain@gmail.com>
2012-10-09 13:26:49 +0100

    nums

--- Nums.hs
+++ Nums.hs
@@ -2,6 +2,8 @@ import Database.HDBC.Sqlite3 (connectSqlite3)
 import Database.HDBC
 import qualified Data.Map as M
 import Text.Printf
+import System.Environment(getArgs)
+import Media.Misc
 
 db    = "/Users/bilalh/Library/Application Support/Media/Media.db"
 
@@ -12,10 +14,19 @@ query = "SELECT Title, Current, Total, Date, Finished, Rewatching, Dropped \
 	\AND Finished = 0 AND (Skip = 0 AND Dropped = 0) \
     \)"
     
-findd = do
+main = do
+    args <- getArgs
+    let days =  parseArgs args
+    nums days
+
+parseArgs :: [String] -> String
+parseArgs [x] =  show (- parseIntCrash x) ++  " days"
+parseArgs _  = "-7 days"
+    
+nums days= do
     conn <- connectSqlite3 db
     stmt <- prepare conn query
-    a <- execute stmt [toSql "-7 days"]
+    a <- execute stmt [toSql days]
     rows <- fetchAllRows stmt
     let rows' = map (map (\b -> fromSql' b))  rows
     printRows rows'
@@ -39,7 +50,7 @@ printRows z@[title:current:total:date:finished:rewatching:dropped:[]]  =  do
         
     -- TODO calcuate length
     -- TODO Change time to BST
-    printf "%s %-*s %3s/%-3s %28s \n"  status (40 :: Int) ( title) ( current) (total) ( date)
+    printf "%s %-*s %3s/%-3s %-28s\n"  status (41 :: Int) ( title) ( current) (total) ( date)
 
 printRows (x:xs) = do
     printRows [x]
