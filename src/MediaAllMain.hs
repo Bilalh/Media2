c2feba438ebc82d0ee4a99283ba2fb2320e416d1
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-01-26 15:57:32 +0000

    Combined the two version of media2 and the --history to record which files have been played

--- src/MediaAllMain.hs
+++ src/MediaAllMain.hs
@@ -17,7 +17,13 @@ path="/Users/bilalh/Movies/.Movies/Anime/"
 
 data VFilter = Oldest    | Latest deriving (Data, Typeable, Show)
 data FFilter = Unwatched | All    deriving (Data, Typeable, Show)
-data Media2 = Media2 {vFilter :: VFilter, fFilter :: FFilter, mPlayer :: PlayerType}
+
+data Media2 = Media2 {
+    vFilter :: VFilter, 
+    fFilter :: FFilter, 
+    vPlayer :: PlayerType,
+    history :: Bool
+    }
     deriving (Data, Typeable, Show)
 
 -- Categorise videos by series and presents a menu for playing them with a player
@@ -37,16 +43,18 @@ getOpts =
             , Latest &= help "Only play the newest file"
             ],
         fFilter = enum
-            [ Unwatched &= help "Only plays file which have not been played (default)"
+            [ Unwatched &= help "Only plays unwatched files (default)"
             , All       &= help "Plays all files"
             ],
-        mPlayer = enum
+        vPlayer = enum
             [ MPlayerOSX &= name "m"  &= help "Use MPlayerOSX as the player (Deafult)"
             , MPlayerX   &= name "x"  &= help "Use MPlayerX as the player"
             , MPlayer    &= name "M"  &= help "Use MPlayer (command line) as the player"
             , VLC        &= name "v"  &= help "Use VLC as the player"
-            ]
+            ],
+        history = def   &= name "h" &= help "Add files to history"
         } &=
+        program "media2" &=
         help "Categorise videos by series and presents a menu for playing." &=
         summary "Media' v2.0 (C) Bilal Syed Hussain" 
 
@@ -56,14 +64,28 @@ vfilterToFunc :: VFilter -> VideoFilter
 vfilterToFunc Oldest = oldest
 vfilterToFunc Latest = latest
 
+
 ffilterToFunc :: FFilter -> FileFilter
 ffilterToFunc Unwatched = unwatched
 ffilterToFunc All       = allMedia
 
+
 play :: Media2 -> IO ()
-play opts@(Media2{vFilter =vf, fFilter=ff, mPlayer=player}) = do
+play opts@(Media2{vFilter =vf, fFilter=ff, vPlayer=player, history=h}) = do
     let vfilter = vfilterToFunc vf
         fFilter = ffilterToFunc ff
     selected <- selectVideosInfo' fFilter path vfilter
     pid <- runCommand $ videoCommand player selected
+    handleHistory h selected
     return ()
+
+
+handleHistory :: Bool -> VideoInfo -> IO ()
+handleHistory True selected = do
+    t <- getCurrentTime
+    addToHistory (series selected) (number selected)  (getTimeStamp  t)
+    runCommand $ labelFile selected "orange"
+    runCommand $ hideExtension selected
+    return ()
+
+handleHistory False _ = return ()
