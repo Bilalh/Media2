6de53c01ca3b4e51f12791790b9322f2dd572719
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-02-09 05:51:29 +0000

    Added space around extra args to players

--- src/Playlist.hs
+++ src/Playlist.hs
@@ -22,7 +22,7 @@ source1 =~+ (source, compOpt, execOpt)
 data Playlists2 = Playlists2
     {vPlayer   :: PlayerType
     ,path      :: FilePath
-    ,extraArgs :: [String]
+    ,extra_args :: [String]
     ,no_default :: Bool
     ,chapter   :: Maybe ((Int,Int))
     ,filter_    :: [String]
@@ -42,11 +42,10 @@ main = do
 
 
 play :: Playlists2 -> IO ()
-play opts@(Playlists2{vPlayer=player, path=p, extraArgs=ea, filter_=f}) = do
+play opts@(Playlists2{vPlayer=player, path=p, extra_args=ea, filter_=f}) = do
    files  <- videos p
    let files2 = filterPaths files f
-   let command = videoCommand (player) files2 (foldl' (++) "" ea)
-   print (foldl' (\a b -> a ++ " " ++ b ) "" ea)
+   let command = videoCommand (player) files2 (foldl' (\a b -> a ++ " " ++ b ) "" ea)
    --print command
 
    pid <- runCommand command
@@ -72,7 +71,7 @@ getOpts =
             , MPlayer    &= name "M"  &= help "Use MPlayer (command line) as the player"
             ]
         ,path       = def &= name "p" &= help "Directory to look for files includes sub dirs " &= typDir
-        ,extraArgs  = def &= name "e" &= help "Extra args to pass to the player"
+        ,extra_args = def &= name "e" &= help "Extra args to pass to the player"
         ,no_default = def &= name "n" &= help "Don't use default mpv args ( -geometry 0%:100% --autofit=480 --loop=inf) "
         ,chapter    = def &= name "c" &= help "Only play the specifed chapters"
         ,filter_    = def &= args &= typ "regex"
@@ -84,14 +83,14 @@ getOpts =
 
 
 processChapter :: Playlists2 -> Playlists2
-processChapter args@(Playlists2{chapter=Just (a,b), extraArgs=ea})  =
-    args{extraArgs= (" --chapter " ++ (show a) ++ "-" ++ (show b) ++ " ") : ea}
+processChapter args@(Playlists2{chapter=Just (a,b), extra_args=ea})  =
+    args{extra_args= (" --chapter " ++ (show a) ++ "-" ++ (show b) ++ " ") : ea}
 
 processChapter args = args
 
 processNodefault :: Playlists2 -> Playlists2
-processNodefault args@(Playlists2{no_default=False, extraArgs=ea})  =
-    args{extraArgs=(playlistArgs ++ argss)  :ea}
+processNodefault args@(Playlists2{no_default=False, extra_args=ea})  =
+    args{extra_args=(playlistArgs ++ argss)  :ea}
 
 processNodefault args = args
 
